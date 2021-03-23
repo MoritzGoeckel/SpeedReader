9abae79b22f5df2c20ae8f7393192b09be736941
Moritz Goeckel <moritz.goeckel@gmail.com>
2021-03-23 01:33:59 +0100

    Add multiple threads for input and display

--- start.py
+++ start.py
@@ -2,6 +2,7 @@ import curses
 import sys
 import math
 import time
+import threading
 
 C_RED = 1
 C_YELLOW = 2
@@ -14,6 +15,9 @@ height, width = stdscr.getmaxyx()
 textWidth = min(57, width)
 leftMargin = math.floor((width - textWidth) / 2)
 topMargin = min(4, math.floor(leftMargin / 6))
+
+wpm = 200
+pause = False
 
 def start():
     global stdscr, C_RED, C_YELLOW
@@ -45,7 +49,7 @@ def getOrp(txt):
 def write(txt):
     global stdscr, height, width, topMargin, leftMargin
     midx = int(round(width / 2))
-    midy = int(round(height / 2))
+    midy = int(round(height / 2)) - 4
 
     # Clear
     stdscr.addstr(midy, midx - 5, "                    ")
@@ -65,14 +69,38 @@ def write(txt):
         stdscr.addstr(midy + 1, midx - orp, txt)
         stdscr.addstr(midy + 1, midx, txt[orp], curses.color_pair(C_RED))
 
-    stdscr.move(0,0)
-    stdscr.refresh()
+    footer = "Running @ " + str(wpm) + " wpm"
+    pos = midx - int(round(len(footer) / 2))
+    stdscr.addstr(midy + 7, pos, footer)
+    stdscr.addstr(midy + 8, pos, "</> to change wpm")
+    stdscr.addstr(midy + 9, pos, "space to pause")
+    stdscr.addstr(midy + 10, pos, "q to quit")
 
-start()
-with open("sample_text.txt") as file:
-    for line in file:
-        for w in line.split():
-            write(w)
-            time.sleep(60 / 500)
-            #stdscr.getch()
-shutdown()
+    stdscr.refresh()
+    stdscr.move(0,0)
+
+def iterateText():
+    global stdscr, wpm, pause
+    start()
+    while True:
+        with open("sample_text.txt") as file:
+            for line in file:
+                for w in line.split():
+                    stdscr.clear()
+                    write(w)
+                    time.sleep(60 / wpm)
+                    while pause: time.sleep(0.05)
+
+reading = threading.Thread(target=iterateText)
+reading.setDaemon(True)
+reading.start()
+
+while True:
+    c = chr(stdscr.getch())
+    if c == '>': wpm += 40
+    if c == '<' and wpm > 100: wpm -= 40
+    if c == ' ': pause = not pause
+    if c == 'q':
+        shutdown()
+        exit()
+
